0fa941d8465667dcea034d76e57d4913bda2c816
Daniel Petranek <dpetranek@flur.ee>
2022-11-11 14:23:45 -0600

    Add static context lookups in js
    Co-authored-by: Marcela Poffald <mpoffald@flur.ee>

--- src/fluree/json_ld/processor/api.cljs
+++ src/fluree/json_ld/processor/api.cljs
@@ -1,33 +1,45 @@
 (ns fluree.json-ld.processor.api
   (:refer-clojure :exclude [flatten])
-  (:require ["jsonld" :as jldjs]))
+  (:require ["jsonld" :as jldjs]
+            [fluree.json-ld.impl.external :as external]))
 
 (defn compact
   [json-ld context]
   (-> json-ld
       (clj->js)
-      (jldjs/compact (clj->js context))
+      (jldjs/compact (clj->js context) #js{"contextUrl" nil, "document" context, "documentUrl" url})
       (.then (fn [result] (js->clj result)))))
+
+(defn static-loader
+  [url options]
+  (js/Promise.
+    (fn [resolve reject]
+      (if-let [context (get external/inlined-contexts url)]
+        (resolve #js{"contextUrl" nil, "document" context, "documentUrl" url})
+        (reject (throw #js{"name" "jsonld.LoadDocumentError"
+                           "message" (str "Unable to load static context: " url)
+                           "details" {"code" "loading remote context failed"
+                                      "url" url}}))))))
 
 (defn expand
   [json-ld]
   (-> json-ld
       (clj->js)
-      (jldjs/expand)
+      (jldjs/expand #js{"documentLoader" static-loader})
       (.then (fn [result] (js->clj result)))))
 
 (defn flatten
   [json-ld]
   (-> json-ld
       (clj->js)
-      (jldjs/flatten)
+      (jldjs/flatten #js{"contextUrl" nil, "document" context, "documentUrl" url})
       (.then (fn [result] (js->clj result)))))
 
 (defn from-rdf
   [n-quads]
   (-> n-quads
       (clj->js)
-      (jldjs/fromRDF #js{"format" "application/n-quads"})
+      (jldjs/fromRDF #js{"format" "application/n-quads"} #js{"contextUrl" nil, "document" context, "documentUrl" url})
       (.then (fn [result] (js->clj result)))))
 
 (defn to-rdf
