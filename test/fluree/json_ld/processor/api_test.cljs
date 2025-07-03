da8b8214a95f66ae30fbfd15dfa190e0ad23d938
Brian Platz <bplatz@gmail.com>
2025-07-03 13:51:47 -0400

    Remove test that is not part of main api or advertised functionality

--- test/fluree/json_ld/processor/api_test.cljs
+++ test/fluree/json_ld/processor/api_test.cljs
@@ -128,21 +128,6 @@
                        (is false (str "Test failed with error: " error))
                        (done))))))
 
-#_(deftest flatten-test
-  (testing "flatten nested structures"
-    (async done
-      (let [nested-doc {"@context" context
-                        "@id" "http://example.org/person/1"
-                        "address" "123 Main St"}]
-        (-> (jld-processor/flatten nested-doc)
-            (.then (fn [result]
-                     ;; Just verify the function works without detailed assertions
-                     (is (or (vector? result) (map? result)))
-                     (done)))
-            (.catch (fn [error]
-                      (is false (str "Test failed with error: " error))
-                      (done))))))))
-
 (deftest canonize
   (async done
          (-> (jld-processor/canonize commit)
