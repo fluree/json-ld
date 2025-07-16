61fca20a1154a4c01206c5dd0b5de2acee9f7f6e
Brian Platz <bplatz@gmail.com>
2025-07-16 11:10:19 -0400

    Fix CI/CD formatting and linting issues
    - Fix cljfmt formatting in crypto_shim.cljs
    - Resolve unused binding warnings by prefixing parameters with underscore
    - All tests passing, CI/CD checks should now pass

--- src/fluree/json_ld/processor/crypto_shim.cljs
+++ src/fluree/json_ld/processor/crypto_shim.cljs
@@ -4,10 +4,10 @@
 ;; Provide a minimal crypto shim for browser environments
 (when (and (exists? js/window) (not (exists? js/crypto)))
   (set! js/global #js{})
-  (set! (.-crypto js/global) 
-        #js{:createHash (fn [algorithm] 
-                          #js{:update (fn [data] 
-                                        #js{:digest (fn [encoding] 
+  (set! (.-crypto js/global)
+        #js{:createHash (fn [_algorithm]
+                          #js{:update (fn [data]
+                                        #js{:digest (fn [_encoding]
                                                       ;; Simple hash fallback
-                                                      (str (.toString (.getTime (js/Date.)) 16) 
+                                                      (str (.toString (.getTime (js/Date.)) 16)
                                                            (.toString (hash data) 16)))})})}))
